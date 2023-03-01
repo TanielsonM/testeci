97a561d1973c215d500ba2ddc7a0e27df17d0ecd
Felipe Barreto <felipebarreto@greenn.com.br>
2023-03-01 14:19:20 -0300

    created function to search country

--- composables/useCountrys.js
+++ composables/useCountrys.js
@@ -1,14 +1,25 @@
 import countrys from "~~/data/countrys";
 
 export const useCountrys = () => {
-  const alphabetical = countrys.sort(function (a, b) {
-    if (a.sub < b.sub) return -1;
-    if (a.sub > b.sub) return 1;
-    return 0;
-  });
+  const alphabetical = countrys
+    .sort(function (a, b) {
+      if (a.sub < b.sub) return -1;
+      if (a.sub > b.sub) return 1;
+      return 0;
+    })
+    .filter((item) => item.show);
+  function searcher(search = "") {
+    return alphabetical.filter(
+      (item) =>
+        item.pais.toLowerCase().includes(search.toLowerCase().trim()) ||
+        item.sub.toLowerCase().includes(search.toLowerCase().trim()) ||
+        item.sigla.toLowerCase().includes(search.toLowerCase().trim())
+    );
+  }
 
   return {
     countrys,
     alphabetical,
+    searcher,
   };
 };
