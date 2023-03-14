a304671995e6871faab91383fa5e811365c0f585
Felipe Barreto <felipebarreto@greenn.com.br>
2023-03-14 14:49:59 -0300

    show coupon infos and total in purchase details

--- components/Purchase/Details.vue
+++ components/Purchase/Details.vue
@@ -1,10 +1,29 @@
 <script setup>
 // Stores
+import { storeToRefs } from "pinia";
 import { useCheckoutStore } from "@/store/checkout";
 // Utils
 import { formatMoney } from "~~/utils/money";
 
 const checkout = useCheckoutStore();
+const {
+  coupon,
+  method,
+  installments,
+  getInstallments,
+  hasFees,
+} = storeToRefs(checkout);
+
+const amountText = computed(() => {
+  switch (method.value) {
+    case "CREDIT_CARD":
+      return `${installments.value}x de ${formatMoney(
+        getInstallments.value()
+      )} ${hasFees.value ? "(Sem juros)" : ""}`;
+    default:
+      return `${formatMoney(getInstallments.value())}`;
+  }
+});
 </script>
 
 <template>
@@ -23,16 +42,33 @@ const checkout = useCheckoutStore();
         <p>{{ item.name }}</p>
         <p>
           {{
-            !item.has_shipping_fee 
+            !item.has_shipping_fee
               ? $t("checkout.pagamento.bump.free")
               : item?.shipping?.amount
-                ? formatMoney(item.shipping.amount)
-                : item?.shipping?.amount === 0
-                ? $t("checkout.pagamento.bump.free")
-                : $t("checkout.pagamento.bump.to_calculate")
+              ? formatMoney(item.shipping.amount)
+              : item?.shipping?.amount === 0
+              ? $t("checkout.pagamento.bump.free")
+              : $t("checkout.pagamento.bump.to_calculate")
           }}
         </p>
       </span>
+    </p>
+    <!-- Coupon -->
+    <p
+      class="flex flex-col items-start gap-1"
+      v-if="coupon.applied && coupon.amount > 0"
+    >
+      <span class="infos-title">{{ $t("checkout.cupom.cupom") }}</span>
+      <span class="infos-content w-full flex justify-between items-center">
+        <p>{{ coupon.name.toUpperCase() }}</p>
+        <p>-{{ formatMoney(coupon.amount) }}</p>
+      </span>
+    </p>
+    <!--  -->
+    <!-- Total -->
+    <p class="flex items-start justify-between gap-1" v-if="coupon.applied">
+      <span class="infos-title">Total</span>
+      <span class="small-text text-xs">{{ amountText }}</span>
     </p>
   </section>
 </template>
