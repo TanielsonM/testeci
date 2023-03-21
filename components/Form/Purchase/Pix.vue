3a3dfb4a84a154ece8f1524c6abe7f8865a2957a
Felipe Barreto <felipebarreto@greenn.com.br>
2023-03-21 14:43:39 -0300

    pix component

---
+++ components/Form/Purchase/Pix.vue
@@ -0,0 +1,38 @@
+<script setup>
+const { t } = useI18n();
+
+const items = ref([
+  {
+    icon: "fa6-solid:bolt",
+    size: 27,
+    title: t("checkout.pagamento.metodos.pix.pix_info1.titulo"),
+    text: t("checkout.pagamento.metodos.pix.pix_info1.texto"),
+  },
+  {
+    icon: "tabler:rectangle-vertical-filled",
+    size: 27,
+    title: t("checkout.pagamento.metodos.pix.pix_info2.titulo"),
+    text: t("checkout.pagamento.metodos.pix.pix_info2.texto"),
+  },
+  {
+    icon: "bi:shield-fill",
+    size: 27,
+    title: t("checkout.pagamento.metodos.pix.pix_info3.titulo"),
+    text: t("checkout.pagamento.metodos.pix.pix_info3.texto"),
+  },
+]);
+</script>
+
+<template>
+  <section class="w-full flex items-start justify-between gap-5">
+    <section
+      class="flex flex-col items-start justify-start gap-3"
+      v-for="(item, index) in items"
+      :key="index"
+    >
+      <Icon :name="item.icon" class="text-main-color h-full" :size="item.size"/>
+      <h4 class="font-semibold text-sm text-txt-color">{{ item.title }}</h4>
+      <p class="font-normal text-[13px] text-txt-color">{{ item.text }}</p>
+    </section>
+  </section>
+</template>
