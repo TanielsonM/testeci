c2144171f317ce26e300a6355d6ab7a692f5e4b4
Felipe Barreto <felipebarreto@greenn.com.br>
2023-03-21 14:43:46 -0300

    ticket component

---
+++ components/Form/Purchase/Boleto.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <p class="text-txt-color text-[13px] font-normal">
+    Pagamentos com Boleto Bancário levam até 3 dias úteis para serem compensados
+    e então terem os produtos liberados.
+  </p>
+</template>
