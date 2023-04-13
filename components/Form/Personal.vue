185d70f616627bc09ddecdde58b7ca5b54d90f17
Felipe Barreto <felipebarreto148@gmail.com>
2023-04-13 17:39:55 -0300

    validation form added

--- components/Form/Personal.vue
+++ components/Form/Personal.vue
@@ -52,44 +52,68 @@ const showPhoneInput = ["BR", "MX", "UY", "AR", "CL"].includes(
 </script>
 
 <template>
-  <form class="grid w-full grid-cols-12 gap-3">
+  <VeeForm class="grid w-full grid-cols-12 gap-3" ref="personal-form">
     <BaseInput
       class="col-span-12"
       :label="$t('forms.personal.inputs.name.label')"
       :placeholder="$t('forms.personal.inputs.name.placeholder')"
+      input-name="name-field"
       v-model="name"
-    />
+      rules="required"
+    >
+      <template #error>
+        {{ $t("checkout.dados_pessoais.feedbacks.nome") }}
+      </template>
+    </BaseInput>
     <BaseInput
       class="col-span-12"
       :label="$t('forms.personal.inputs.mail.label')"
       :placeholder="$t('forms.personal.inputs.mail.placeholder')"
       type="email"
+      input-name="email-field"
       v-model="email"
-    />
+      rules="required|email"
+    >
+      <template #error>
+        {{ $t("checkout.dados_pessoais.feedbacks.email") }}
+      </template>
+    </BaseInput>
     <BaseInput
       class="col-span-12"
       :label="$t('forms.personal.inputs.confirmation_mail.label')"
       :placeholder="$t('forms.personal.inputs.confirmation_mail.placeholder')"
       type="email"
       :autocomplete="false"
+      input-name="confirmation_mail-field"
       v-model="confirmation_mail"
+      rules="required|email|confirmed:@email-field"
       v-if="custom_checkout.hasConfirmationEmail"
-    />
+    >
+      <template #error>
+        {{ $t("checkout.dados_pessoais.feedbacks.confirmation_email") }}
+      </template>
+    </BaseInput>
     <BaseInput
       class="col-span-12"
       :class="{ 'xl:col-span-6': showPhoneInput }"
       :label="$t('forms.personal.inputs.cellphone.label')"
       :placeholder="$t('forms.personal.inputs.cellphone.placeholder')"
+      input-name="cellphone-field"
       v-model="cellphone"
       type="tel"
-    />
+    >
+      <template #error>
+        {{ $t("checkout.dados_pessoais.feedbacks.celular") }}
+      </template>
+    </BaseInput>
     <BaseInput
       class="col-span-12 xl:col-span-6"
       v-if="showPhoneInput"
       :label="documentText.label"
       :placeholder="documentText.placeholder"
+      input-name="document-field"
       v-model="document"
       :mask="documentText.mask"
     />
-  </form>
+  </VeeForm>
 </template>
