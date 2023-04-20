cbb23020da5c2dd5744556e4bada1737b9f8f639
Felipe Barreto <felipebarreto148@gmail.com>
2023-04-20 15:24:50 -0300

    fixed installments module

--- store/modules/installments.ts
+++ store/modules/installments.ts
@@ -31,6 +31,7 @@ export const useInstallmentsStore = defineStore("installments", {
         let frete = 0;
 
         product_list.value.map((item: Product) => {
+          let value = item.amount;
           // Verifica se produto tem frete
           if (!!item.has_shipping_fee) {
             frete +=
@@ -39,18 +40,18 @@ export const useInstallmentsStore = defineStore("installments", {
                 : item.shipping?.amount || 0;
           }
           // Verifica se tem cupom
-          if (item.id === product_id.value && coupon.value.applied) {
-            item.amount -= coupon.value.amount;
+          if (item.id === parseInt(product_id.value) && coupon.value.applied) {
+            value -= coupon.value.amount;
           }
           // Cliente não paga juros
           if (!item.no_interest_installments) {
-            amount += item.amount;
+            amount += value;
           }
           // Cliente paga juros
           else {
             let i = parseFloat(monthly_interest.value) / 100;
             amount +=
-              (item.amount * n) /
+              (value * n) /
               ((Math.pow(i + 1, n) - 1) / (Math.pow(i + 1, n) * i));
           }
         });
