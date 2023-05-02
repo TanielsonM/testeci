f2eb99012c139a3c4d4589d1e364c1857f1fe0c1
wladi1 <wladi@greenn.com.br>
2023-05-02 15:22:02 -0300

    chore: remove import ignored

--- pages/[product_id]/index.vue
+++ pages/[product_id]/index.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { storeToRefs } from "pinia";
 import { useProductStore } from "~~/store/product";
 import { useCheckoutStore } from "~~/store/checkout";
 import { useAddressStore } from "@/store/forms/address";
