2abe7a5ea20ebf95c9bf550d9cb8259a4aaa3859
Felipe Barreto <felipebarreto148@gmail.com>
2023-05-05 11:33:53 -0300

    set custom logo

--- components/Base/Header.vue
+++ components/Base/Header.vue
@@ -1,41 +1,31 @@
 <script setup>
 import { useCustomCheckoutStore } from "~~/store/customCheckout";
-import { useCheckoutStore } from "~~/store/checkout";
 import { useProductStore } from "~~/store/product";
 import greenn from "../../assets/logos/logo.png";
 import heaven from "../../assets/heaven/logo.svg";
 
-const custom_checkout = useCustomCheckoutStore();
-const checkout = useCheckoutStore();
+const customCheckStore = useCustomCheckoutStore();
 const product = useProductStore();
+
+const { topThumb, hasScarcity, hasCustomLogo } = storeToRefs(customCheckStore);
 </script>
 
 <template>
   <Scarcity v-if="product.isValid()" />
   <header
-    v-if="custom_checkout.topThumb && product.isValid()"
+    v-if="topThumb && product.isValid()"
     class="-mt-8 flex max-h-[40vh] w-full items-center justify-center"
   >
-    <img :src="custom_checkout.topThumb" alt="Thumb superior" />
+    <img :src="topThumb" alt="Thumb superior" />
   </header>
   <header
-    v-if="
-      !custom_checkout.hasScarcity &&
-      !custom_checkout.topThumb &&
-      product.isValid()
-    "
+    v-if="!hasScarcity && !topThumb && product.isValid()"
     class="sticky top-0 z-50 flex min-h-[60px] w-full items-center bg-checkout px-4 shadow-lg"
   >
     <img
-      :src="
-        custom_checkout.customLogotipo
-          ? custom_checkout.customLogotipo
-          : checkout.isHeaven
-          ? heaven
-          : greenn
-      "
+      :src="hasCustomLogo ? hasCustomLogo : greenn"
       alt="Logo da pagina"
-      class="max-h-10 max-w-[100px]"
+      class="max-h-10 max-w-[100px] object-contain"
       width="100"
       height="40"
     />
