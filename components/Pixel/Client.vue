80ef81feb2dd1b7230bcfee1fb5a91aa708ca49d
wladi1 <wladi@greenn.com.br>
2023-06-28 12:26:38 -0300

    fix: add gtag rendering function to pixel

--- components/Pixel/Client.vue
+++ components/Pixel/Client.vue
@@ -30,6 +30,16 @@ onMounted(async () => {
       const { event_id, pixels } = response;
 
       if (pixels && pixels.length) {
+        const hasTagManager = pixels.filter(
+          (item) => item.type == "GOOGLETAGMANAGER"
+        );
+
+        if (hasTagManager.length) {
+          hasTagManager.forEach((item) => {
+            handleGtag(item.pixel_id);
+          });
+        }
+
         pixels.forEach((pixel) => {
           handleIframe(
             pixel.host,
@@ -46,6 +56,19 @@ onMounted(async () => {
         });
       }
     });
+
+    function handleGtag(pixel_id: any) {
+      (function (w: any, d: any, s: any, l: any, i: any) {
+        w[l] = w[l] || [];
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, "script", "dataLayer", pixel_id);
+    }
 
     function handleIframe(
       host: string,
