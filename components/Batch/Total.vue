3bb73fabfb9f41cfd61fa47dfc68b7017ac46bca
SandroSouza <sandro@greenn.com.br>
2023-07-11 16:07:19 -0300

    feat: total dinamico

--- components/Batch/Total.vue
+++ components/Batch/Total.vue
@@ -1,7 +1,21 @@
+<script setup>
+import { useAmountStore } from "~~/store/modules/amount";
+import { useInstallmentsStore } from "~~/store/modules/installments";
+import { useCheckoutStore } from "~~/store/checkout";
+
+const amountStore = useAmountStore();
+const installmentsStore = useInstallmentsStore();
+const checkout = useCheckoutStore();
+
+const { amount } = storeToRefs(amountStore);
+const { getInstallments } = storeToRefs(installmentsStore);
+const { installments } = storeToRefs(checkout);
+</script>
+
 <template>
   <div>
     <small class="text-gray-400">Total</small>
-    <p class="text-3xl text-input-color font-bold">R$ 671,00</p>
-    <small class="text-main-color">ou 12x de R$ 69,40</small>
+    <p class="text-3xl text-input-color font-bold">{{ formatMoney(amount) }}</p>
+    <small class="text-main-color">ou {{ installments }}x de {{ formatMoney(getInstallments()) }}</small>
   </div>
 </template>
