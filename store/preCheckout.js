1ea84f180b5d60ed14640a6eea54564cded45099
SandroSouza <sandro@greenn.com.br>
2023-07-14 09:09:29 -0300

    feat: ajustes funcoes de requisicoes

--- store/preCheckout.js
+++ store/preCheckout.js
@@ -52,7 +52,7 @@ export const usePreCheckoutStore = defineStore("preCheckout", {
       this.reservations.push(value);
     },
     updateReservation(value) {
-      const reservation = this.reservations.find(x => x.id === value.id)
+      const reservation = this.reservations.find(x => x.id === value.id);
       const index = this.reservations.indexOf(reservation);
       if(index !== -1) {
         this.reservations = this.reservations.splice(index, 1, value);
@@ -73,32 +73,31 @@ export const usePreCheckoutStore = defineStore("preCheckout", {
         console.log(this.reservations, this.reservations?.length && this.reservations.some(x => x.offer_id === batch.id))
         if(this.reservations?.length && this.reservations.some(x => x.offer_id === batch.id)) {
           // Edita a reserva do lote existente com a nova quantidade de ingressos selecionados
-          await this.putReservation(batch);
-          this.updateReservation(payload)
-          localStorage.setItem('reservations', JSON.stringify(this.reservations))
+          const res = await this.putReservation(batch);
+          this.updateReservation(res);
+          localStorage.setItem('reservations', JSON.stringify(this.reservations));
         } else {
           // Cria nova reserva do ingresso do lote selecionado
           await this.createReservation(batch.id);
-          this.addReservation({...res, offer_id: batch.id})
-          localStorage.setItem('reservations', JSON.stringify(this.reservations))
+          localStorage.setItem('reservations', JSON.stringify(this.reservations));
         }
       }
     },
     async subTicket(hash) {
-      let batch = this.batchs_list.find(x => x.hash === hash)
+      let batch = this.batchs_list.find(x => x.hash === hash);
       if(batch?.selected_tickets > 0 && saleHasStarted(batch)) {
         batch.selected_tickets -= 1;
         const checkoutStore = useCheckoutStore();
         checkoutStore.removeProductList(batch);
         if(batch.selected_tickets === 0) {
           // Deleta a reserva do lote existente, já que foram removidos todos ingressos
-          const reservation = this.reservations.find(x => x.offer_id === batch.id)
-          await this.deleteReservation(reservation)
+          const reservation = this.reservations.find(x => x.offer_id === batch.id);
+          await this.deleteReservation(reservation);
           localStorage.setItem('reservations', JSON.stringify(this.reservations));
         } else {
           // Edita a reserva do lote existente com a nova quantidade de ingressos selecionados
-          await this.putReservation(batch);
-          this.updateReservation(payload)
+          const res = await this.putReservation(batch);
+          this.updateReservation(res);
           localStorage.setItem('reservations', JSON.stringify(this.reservations))
         }
       }
@@ -110,31 +109,32 @@ export const usePreCheckoutStore = defineStore("preCheckout", {
       const payload = { start, end, offer_id, quantity: 1 };
       console.log("create payload", payload)
       try {
-        const res = await useApi().create('/event/reservation', payload)
+        const res = await useApi().create('/event/reservation', payload);
         console.log("create res", res)
+        this.addReservation({ ...res, offer_id });
         return res;
       } catch(err) {
-        console.error(err)
+        console.error(err);
         return err;
       }
     },
     async putReservation(batch) {
-      const reservation = this.reservations.find(x => x.offer_id === batch.id)
-      const payload = { ...reservation, quantity: batch.selected_tickets }
-      console.log("delete reservation", payload)
+      const reservation = this.reservations.find(x => x.offer_id === batch.id);
+      const payload = { ...reservation, quantity: batch.selected_tickets };
+      console.log("update reservation", payload)
       try {
-        const res = await useApi().update(`/event/reservation/${payload.token}`, payload)
+        const res = await useApi().update(`/event/reservation/${payload.token}`, payload);
         console.log("update res", res)
         return res;
       } catch(err) {
-        console.error(err)
+        console.error(err);
         return err;
       }
     },
     async deleteReservation(reservation) {
       console.log("delete reservation", reservation)
       try {
-        const res = await useApi().remove(`/event/reservation/${reservation.token}`)
+        const res = await useApi().remove(`/event/reservation/${reservation.token}`);
         console.log("delete res", res)
         if(this.reservations?.length) this.removeReservation(reservation);
         return res;
