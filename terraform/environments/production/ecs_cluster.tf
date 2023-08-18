3d0cdb6ee51cc4b8e4c8372a81c60cf53df68982
danilomacedo1308 <danilomacedo1308@gmail.com>
2023-08-18 10:23:56 -0300

    node-v2

--- terraform/environments/production/ecs_cluster.tf
+++ terraform/environments/production/ecs_cluster.tf
@@ -11,17 +11,3 @@ resource "aws_ecs_cluster" "node" {
     base              = 1
   }
 }
-
-resource "aws_ecs_cluster" "node_v2" {
-  name               = "node_v2"
-  capacity_providers = ["FARGATE"]
-  # default_capacity_provider_strategy {
-  #   capacity_provider = "FARGATE_SPOT"
-  #   weight            = 10
-  # }
-  default_capacity_provider_strategy {
-    capacity_provider = "FARGATE"
-    weight            = 1
-    base              = 1
-  }
-}
