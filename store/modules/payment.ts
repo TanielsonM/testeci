faa353ca441c1c494342a7ff59e01c44e3797227
SandroSouza <sandro@greenn.com.br>
2023-08-30 10:00:09 -0300

    feat: receber stato do campo de endereco ao em vez de documento quando EUA

--- store/modules/payment.ts
+++ store/modules/payment.ts
@@ -124,8 +124,7 @@ export const usePaymentStore = defineStore("Payment", {
         complement: charge.value.complement,
         neighborhood: charge.value.neighborhood,
         city: charge.value.city,
-        state:
-          selectedCountry.value === "US" ? document.value : charge.value.state,
+        state: charge.value.state,
         // Others
         language,
         upsell_id: hasUpsell.value,
