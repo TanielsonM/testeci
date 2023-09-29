d40dc4bc00c37d193cd3d413177b5a30c47c7db4
Felipe Barreto <felipebarreto@greenn.com.br>
2023-09-29 09:28:24 -0300

    feat: mutex removed

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -3,14 +3,12 @@ import { HeadersState } from "@/types";
 
 // Utils
 import { GreennLogs } from "@/utils/greenn-logs";
-import { Mutex } from 'async-mutex';
 import { useLoadingStore } from "@/store/loading/loading";
 
 const loading = useLoadingStore();
 const headStore = useHeadersStore();
 
 export default function () {
-  const apiMutex = new Mutex();
 
   async function instance<T>(
     url: string,
@@ -19,62 +17,60 @@ export default function () {
     body: any = null
   ): Promise<T | any> {
     if (body) config = { body };
-    const { data, error } = await apiMutex.runExclusive(async () => {
-      return await useFetch<T>(url, {
-        ...config,
-        method,
-        baseURL: useRuntimeConfig().public.API_BASE_URL,
-        onRequest({ request, options }) {
-          loading.changeLoading(request.toString());
-          const headers: HeadersInit = new Headers();
-          headers.set("Content-type", "application/json");
-          if (request === "/payment") {
-            // controller-token-
-            if (headStore["controller-token-"]) {
-              headers.set("Controller-Token-", headStore["controller-token-"]);
-            }
-            // requestray-token-
-            if (headStore["requestray-token-"]) {
-              headers.set("RequestRay-Token-", headStore["requestray-token-"]);
-            }
-            // firewall-token-
-            if (headStore["firewall-token-"]) {
-              headers.set("Firewall-Token-", headStore["firewall-token-"]);
-            }
-            // cache-token-
-            if (headStore["cache-token-"]) {
-              headers.set("Cache-Token-", headStore["cache-token-"]);
-            }
-            // trans-token-
-            if (headStore["trans-token-"]) {
-              headers.set("Trans-Token-", headStore["trans-token-"]);
-            }
-            // wd-token-
-            if (headStore["wd-token-"]) {
-              headers.set("Wd-Token-", headStore["wd-token-"]);
-            }
-
-            GreennLogs.logger.info("axiosRequest", {
-              axiosRequest: options,
-            });
+    const { data, error } = await useFetch<T>(url, {
+      ...config,
+      method,
+      baseURL: useRuntimeConfig().public.API_BASE_URL,
+      onRequest({ request, options }) {
+        loading.changeLoading(request.toString());
+        const headers: HeadersInit = new Headers();
+        headers.set("Content-type", "application/json");
+        if (request === "/payment") {
+          // controller-token-
+          if (headStore["controller-token-"]) {
+            headers.set("Controller-Token-", headStore["controller-token-"]);
           }
-          options.headers = headers;
-        },
-        onResponse({ request, response }) {
-          if (request.toString().includes("/api/product")) {
-            const headers: HeadersState = {
-              "controller-token-": response.headers.get("controller-token-"),
-              "requestray-token-": response.headers.get("requestray-token-"),
-              "firewall-token-": response.headers.get("firewall-token-"),
-              "cache-token-": response.headers.get("cache-token-"),
-              "trans-token-": response.headers.get("trans-token-"),
-              "wd-token-": "",
-            };
-
-            headStore.updateHeaders(headers);
+          // requestray-token-
+          if (headStore["requestray-token-"]) {
+            headers.set("RequestRay-Token-", headStore["requestray-token-"]);
           }
-        },
-      });
+          // firewall-token-
+          if (headStore["firewall-token-"]) {
+            headers.set("Firewall-Token-", headStore["firewall-token-"]);
+          }
+          // cache-token-
+          if (headStore["cache-token-"]) {
+            headers.set("Cache-Token-", headStore["cache-token-"]);
+          }
+          // trans-token-
+          if (headStore["trans-token-"]) {
+            headers.set("Trans-Token-", headStore["trans-token-"]);
+          }
+          // wd-token-
+          if (headStore["wd-token-"]) {
+            headers.set("Wd-Token-", headStore["wd-token-"]);
+          }
+
+          GreennLogs.logger.info("axiosRequest", {
+            axiosRequest: options,
+          });
+        }
+        options.headers = headers;
+      },
+      onResponse({ request, response }) {
+        if (request.toString().includes("/api/product")) {
+          const headers: HeadersState = {
+            "controller-token-": response.headers.get("controller-token-"),
+            "requestray-token-": response.headers.get("requestray-token-"),
+            "firewall-token-": response.headers.get("firewall-token-"),
+            "cache-token-": response.headers.get("cache-token-"),
+            "trans-token-": response.headers.get("trans-token-"),
+            "wd-token-": "",
+          };
+
+          headStore.updateHeaders(headers);
+        }
+      },
     });
 
     if (error.value?.statusCode === 500) {
