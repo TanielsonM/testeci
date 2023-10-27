48198a7c06813614c76b0cf8bb3da0db2bb4601f
glpat-TqiFssEKdx2DyxPxzEbg <glpat-TqiFssEKdx2DyxPxzEbg>
2023-10-27 17:34:25 -0300

    feat: redirect product heaven

--- store/checkout.js
+++ store/checkout.js
@@ -229,6 +229,20 @@ export const useCheckoutStore = defineStore("checkout", {
             ...configs,
             query,
           })
+          .then(res => {
+            const product = res.data;
+
+            if(product.offer_redirect_id) {
+              const urlAtual = new URL(window.location.href);
+              const parametros = urlAtual.pathname;
+              const queries = urlAtual.search;
+              const novaRota = useRuntimeConfig().public.HEAVEN_CHECKOUT_PAGE;
+              const novaUrl = `${novaRota}${parametros}${queries}`;
+              window.location.href = novaUrl;
+            }
+
+            return res;
+          })
           .then(async (response) => {
             if(response.allow_free_offers){
               this.setAllowFreeOffers(response.allow_free_offers)
