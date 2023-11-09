7df54dca8f3365c9a534e07454c2fe7ad076bbfa
glpat-TqiFssEKdx2DyxPxzEbg <glpat-TqiFssEKdx2DyxPxzEbg>
2023-11-09 00:10:37 -0300

    fix: valor dos dois cartoes com arredondamento correto

--- store/modules/installments.ts
+++ store/modules/installments.ts
@@ -59,6 +59,7 @@ export const useInstallmentsStore = defineStore("installments", {
               ((Math.pow(i + 1, n) - 1) / (Math.pow(i + 1, n) * i));
           }
         });
+        total = Math.round(total * 100) / 100;
         return Number(Number((total + frete) / n));
       };
     },
