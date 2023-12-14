c5d00bcb43ee6fb69c6a31ae5efecb87256520ff
llgod <allangdy2594@gmail.com>
2023-12-14 18:16:09 -0300

    ops: scale

--- terraform/environments/production/ecs_service_node.tf
+++ terraform/environments/production/ecs_service_node.tf
@@ -37,8 +37,8 @@ resource "aws_ecs_service" "node" {
 
 
 resource "aws_appautoscaling_target" "node_target" {
-  max_capacity       = 20
-  min_capacity       = 2 #normal
+  max_capacity       = 64
+  min_capacity       = 20 #normal
   # min_capacity       = 8 #lancamento
   resource_id        = "service/${aws_ecs_cluster.node.name}/${aws_ecs_service.node.name}"
   scalable_dimension = "ecs:service:DesiredCount"
