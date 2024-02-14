6baa58b1dd4e58cf73e443a23dc973719d288820
Gustavo Grativol <graweb1@gmail.com>
2024-02-14 15:39:51 -0300

    Format product description (order bump)

--- components/OrderBumps/Body.vue
+++ components/OrderBumps/Body.vue
@@ -4,6 +4,10 @@ import { useCustomCheckoutStore } from "~~/store/customCheckout";
 // Utils
 import { formatMoney } from "~/utils/money";
 import { useProductStore } from "~~/store/product";
+
+import { MdPreview } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
 const productStore = useProductStore();
 const { product } = storeToRefs(productStore);
 
@@ -116,7 +120,7 @@ function getType(type = "") {
       </section>
       <!-- More product infos -->
       <section class="right__side" :class="`${bump.type.toLowerCase()}`">
-        <h1 class="item-title text-txt-color">{{ bump.name }}</h1>
+        <h1 class="item-title text-txt-color ml-5">{{ bump.name }}</h1>
         <template v-if="hasTrial">
           <p class="info-value custom-color">
             {{ trialMessage }}
@@ -130,7 +134,7 @@ function getType(type = "") {
           <section class="hide-details" v-if="!details" key="hide-details">
             <span
               v-if="!hasTrial && !hasCustomCharges"
-              class="info-value custom-color"
+              class="info-value custom-color ml-5"
               >{{ `${formatMoney(amount)}` }}</span
             >
             <section class="charges" :opened="details" v-if="hasCustomCharges && !exceptionSellerId">
@@ -202,7 +206,7 @@ function getType(type = "") {
           </section>
         </Transition>
         <span class="item-description" v-if="showDescription">
-          {{ bump.description }}
+          <MdPreview v-model="bump.description" style="background-color: transparent;" />
         </span>
       </section>
     </section>
