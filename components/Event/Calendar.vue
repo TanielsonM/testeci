15c5fc2286de786fab1760ebca06dbc33767192b
JoãoRamos <glpat-PW65yftSY1FC6WixGig7>
2024-02-16 13:54:18 -0300

    feat-ajust-date

--- components/Event/Calendar.vue
+++ components/Event/Calendar.vue
@@ -3,15 +3,17 @@ import moment from "@/plugins/moment";
 import { useProductStore } from "~~/store/product";
 
 const { product } = useProductStore();
+const startDateMonth = product.start_date ? (moment(product.start_date).format('MMM')).toUpperCase() : '---';
+const startDateDay = product.start_date ? moment(product.start_date).format('DD') : 'X';
 </script>
 
 <template>
   <div class="w-[85px] h-[94px] border border-[#E5E5E5] rounded-lg">
     <div class="w-full h-[31px] bg-main-color rounded-t-lg text-center">
-      <span class="text-[14px] font-[600] text-white">{{ (moment(product?.start_date).format('MMM')).toUpperCase() }}</span>
+      <span class="text-[14px] font-[600] text-white">{{ startDateMonth }}</span>
     </div>
     <div class="w-full h-[63px] flex justify-center items-center">
-      <span class="text-[32px] font-[700] text-input-color">{{ moment(product?.start_date).format('DD') }}</span>
+      <span class="text-[32px] font-[700] text-input-color">{{ startDateDay }}</span>
     </div>
   </div>
 </template>
--- components/Event/Info.vue
+++ components/Event/Info.vue
@@ -3,6 +3,11 @@ import moment from "@/plugins/moment";
 import { useProductStore } from "~~/store/product";
 
 const { product } = useProductStore();
+const startDateDDD = product.start_date ? moment(product.start_date).format('ddd') : null;
+const startDateLL = product.start_date ? moment(product?.start_date).format('LL') : null;
+const startDateConcat = startDateDDD && startDateLL ? `${startDateDDD},${startDateLL}` : 'Configure a data e local do evento';
+const startDateTime =  product.start_date ? moment(product.start_date+' '+product.start_time).format('HH:mm') : 'Configure a data e local do evento';
+const location = product.location ? product.location : 'Configure a data e local do evento';
 </script>
 
 <template>
@@ -10,18 +15,17 @@ const { product } = useProductStore();
     <li class="flex items-center mb-5">
       <img class="mr-3" width="18" src="@/assets/icons/calendar.svg" alt="calendar">
       <span>
-        {{ moment(product?.start_date).format('ddd') }},
-        {{ moment(product?.start_date).format('LL') }}
+        {{ startDateConcat }}
       </span>
     </li>
     <li class="flex items-center mb-5">
       <img class="mr-3" width="18" src="@/assets/icons/clock.svg" alt="clock">
-      <span>{{ moment(product?.start_date+' '+product?.start_time).format('HH:mm') }} (horário de Brasília)</span>
+      <span>{{ startDateTime }} (horário de Brasília)</span>
     </li>
     <li class="flex items-center mb-5">
       <img class="mr-3" width="18" src="@/assets/icons/location.svg" alt="location">
       <span>
-        {{ product?.location }}
+        {{ location }}
       </span>
     </li>
   </ul>
