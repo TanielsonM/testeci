75837f411e75c2a3b346829e810bbe8b664afb5e
Gustavo Ajada <gustavo.gonzales@greenn.com.br>
2024-02-28 19:57:01 -0300

    Corrige a contagem de ingressos quando colocado o máximo de itens

--- components/Batch/List.vue
+++ components/Batch/List.vue
@@ -108,14 +108,14 @@ const getSmallerAmount = function (tickets) {
       </div>
       <ul v-if="!dependsOnAnotherBatch(batch) && saleHasStarted(batch)" class="text-txt-color">
         <li v-for="(ticket, i) in batch.tickets" :key="ticket?.hash" class="mb-6 pt-5 flex justify-between items-center border-[#E5E5E5]" :class="{ 'border-t': i !== 0 }">
-          <div class="ml-5" :class="{ 'line-through': !haveAvailableTickets(batch) }">
+          <div class="ml-5" :class="{ 'line-through': !haveAvailableTickets(batch) || !preCheckout.hasAvailableTickets }"> 
             <h5 class="text-[18px] font-bold text-input-color mb-2">{{ ticket?.name }}</h5>
             <p class="text-[16px] font-[400] text-txt-color">{{ formatMoney(ticket?.amount) }}</p>
             <small v-if="ticket?.selected_tickets > 0" class="text-[14px] font-[400] text-main-color">
-              {{ $t("pre_checkout.in_until") }} {{ ticket?.max_installments ?? 12 }}x de {{ formatMoney(getTicketInstallments(batch, ticket?.hash)) }}
+              {{ $t("pre_checkout.in_until") }} G {{ ticket?.max_installments ?? 12 }}x de {{ formatMoney(getTicketInstallments(batch, ticket?.hash)) }}
             </small>
           </div>
-          <div v-if="preCheckout.hasAvailableTickets && (batch.available_tickets > 0 || batch.release_type === 'fixed_date') && haveAvailableTickets(batch) || (!haveAvailableTickets(batch) && ticket?.selected_tickets > 0)" class="flex items-center mr-5">
+          <div v-if="(batch.available_tickets > 0 || batch.release_type === 'fixed_date') && haveAvailableTickets(batch) || (!haveAvailableTickets(batch) && ticket?.selected_tickets > 0)" class="flex items-center mr-5">
             <template v-if="!ticket.load">
               <Icon name="mdi:minus-circle-outline" size="20" :class="{
                 'text-gray-300': ticket?.selected_tickets === 0 || !saleHasStarted(batch),
