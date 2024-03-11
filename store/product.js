6977e9d62cd9b01b0023d147d5cca0538ccca9f4
Allan Godoy <allangdy2594@gmail.com>
2024-03-11 12:21:30 +0000

    Revert "ix: habilitado o paypal no checkout"
    This reverts commit 64af251db84959b1a98e0b095017ab1a6299d4c3

--- store/product.js
+++ store/product.js
@@ -141,7 +141,10 @@ export const useProductStore = defineStore("product", {
       }
       let allowed_methods = product.method.split(",");
 
-      if (
+      if(product.method.includes("PAYPAL") && !this.product.is_heaven){
+        allowed_methods = allowed_methods.filter((item) => item != "PAYPAL");
+
+      }else if (
         !!product.seller.is_heaven &&
         product.method.includes("PAYPAL") &&
         checkout.selectedCountry !== "BR"
