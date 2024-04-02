c0f2d3a4552f8d1b05e788beb54b0a00c2d2279d
Kevin Luiz Storti <kevin.storti@greenn.com.br>
2024-04-02 15:05:06 -0300

    fix: removed google tag manager

--- components/Pixel/Client.vue
+++ components/Pixel/Client.vue
@@ -31,24 +31,7 @@ onMounted(async () => {
       const { event_id, pixels } = response;
 
       if (pixels && pixels.length) {
-        const hasTagManager = pixels.filter(
-          (item) => item.type == "GOOGLETAGMANAGER"
-        );
-
-        if (hasTagManager.length) {
-          hasTagManager.forEach((item) => {
-            handleGtag(item.pixel_id, item.product_id, item.user_id, item.host);
-
-            if (props.event === 'view') {
-              onCheckoutEvent();
-            }
-
-            if (props.event === 'conversion') {
-              onPurchaseEvent();
-            }
-          });
-        }
-
+        
         pixels.forEach((pixel) => {          
           handleIframe(
             pixel.host,
@@ -65,68 +48,6 @@ onMounted(async () => {
         });
       }
     });
-
-    function onCheckoutEvent() {
-      dataLayer.push({
-        event: 'checkout',
-        checkout: {
-          products: [
-            {
-              amount: props.amount || props.original_amount,
-              product_name: props.product_name,
-              product_id: props.product_id,
-            },
-          ],
-        },
-      });
-    }
-
-    function onPurchaseEvent() {
-      dataLayer.push({
-        event: "purchase",
-        purchase: {
-          sale_id: props.sale_id,
-          method: props.method,
-          products: [
-            {
-            amount: props.amount || props.original_amount,
-            product_id: props.product_id,
-            product_name: props.product_name,
-            }
-          ],
-        },
-      });
-    }
-  
-    function handleGtag(pixel_id: string, product_id: number, user_id: number, host: string,) {
-      (function (w: any, d: any, s: any, l: any, i: any) {
-        w[l] = w[l] || [];
-        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-        var f = d.getElementsByTagName(s)[0],
-          j = d.createElement(s),
-          dl = l != "dataLayer" ? "&l=" + l : "";
-        j.async = true;
-        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-        f.parentNode.insertBefore(j, f);
-      })(window, document, "script", "dataLayer", pixel_id);
-
-      let data = {
-        column: props.event === 'view' ? 'view_events_triggered_count' : 'conversion_events_triggered_count',
-        seller_id: `${user_id}`,
-        pixel_id: `${pixel_id}`,
-        product_id: `${product_id}`,
-    };
-
-    fetch(`https://${host}/api/lexip/metrics`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-
-      
-    }
 
     function handleIframe(
       host: string,
