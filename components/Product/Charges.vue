4f01d744e0d2f11f3b7a46c8cc61063f27ef6d0c
Lucas Sahdo <lucas.sahdo@greenn.com.br>
2024-05-06 15:36:52 -0300

    feat: adicionando sulfixo ao valor de cobrança

--- components/Product/Charges.vue
+++ components/Product/Charges.vue
@@ -9,7 +9,7 @@ const { history_subscription } = storeToRefs(checkout);
 
 <template>
   <ClientOnly>
-    {{ (history_subscription.total_charges_paid + 1) + ' '  + $t("order.cobranca") }}
+    {{ (history_subscription.total_charges_paid + 1) + $t("order.cobranca") }}
     <template v-if="history_subscription.contract_charges">
       {{ ' ' + $t("order.de") + ' ' + history_subscription.contract_charges }}
     </template>
