b0ce50d388cdd75c1a8b3ce370ca94ef7ed225f0
Sandro Souza <sandro@greenn.com.br>
2024-05-21 21:06:48 -0300

    fix: ajuste coupon aparece onde n deveria

--- components/Product/Card.vue
+++ components/Product/Card.vue
@@ -269,7 +269,7 @@ const renovation =
       <!-- Coupon -->
       <ProductCoupon 
         :urlSubscription="urlSubscription"
-        v-if="productStore.allowedCoupon && !sellerHasFeatureTickets"
+        v-if="(urlSubscription && history_subscription.coupon) || (productStore.allowedCoupon && !sellerHasFeatureTickets)"
       />
       <ProductCashback />
     </section>
