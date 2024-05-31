da854186b608a8ad9b6ffe93196f070819f52b1f
Sandro Souza <sandro@greenn.com.br>
2024-05-31 15:26:53 -0300

    improvement: identacao fingerprint

--- composables/useFingerprint.ts
+++ composables/useFingerprint.ts
@@ -2,33 +2,33 @@ import FingerprintJS from '@fingerprintjs/fingerprintjs-pro';
 import { useRuntimeConfig } from 'nuxt/app';
 
 export const useFingerprint = async () => {
-    const response = ref<null | string>(null);
-    const config = useRuntimeConfig();
-    if (process.client) {
-        try {
-            const fpPromise = FingerprintJS.load({
-                apiKey: config.public.FINGERPRINT_API_KEY,
-                endpoint: [
-                    config.public.FINGERPRINT_ENDPOINT
-                  ],
-                  scriptUrlPattern: [
-                    `${config.public.FINGERPRINT_PATH}?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>`
-                  ]
-            });
-            const { requestId } = await (await fpPromise).get();
-            response.value = requestId
-        } catch (error) {
-            const errorMessage = error.message || 'Unknown error';
-            GreennLogs.logger.error("ErrorFingerPrint", {
-                name: "ErrorFingerPrint",
-                error_code: error.code || null,
-                error_message: errorMessage,
-            });
-            response.value = 'fingerprint_error';
-        }
+  const response = ref<null | string>(null);
+  const config = useRuntimeConfig();
+  if (process.client) {
+    try {
+      const fpPromise = FingerprintJS.load({
+        apiKey: config.public.FINGERPRINT_API_KEY,
+        endpoint: [
+          config.public.FINGERPRINT_ENDPOINT
+        ],
+        scriptUrlPattern: [
+          `${config.public.FINGERPRINT_PATH}?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>`
+        ]
+      });
+      const { requestId } = await (await fpPromise).get();
+      response.value = requestId
+    } catch (error) {
+      const errorMessage = error.message || 'Unknown error';
+      GreennLogs.logger.error("ErrorFingerPrint", {
+        name: "ErrorFingerPrint",
+        error_code: error.code || null,
+        error_message: errorMessage,
+      });
+      response.value = 'fingerprint_error';
     }
+  }
 
-    return { requestId: response.value };
+  return { requestId: response.value };
 };
 
 
