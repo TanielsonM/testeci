29aa1f6f393fb9595c502c6786d37b6dc40c16f7
Lucca Bastos <lucca.bastos@clouddog.com.br>
2024-07-18 08:43:14 -0300

    Change: Services

---
+++ infrastructure/stg/new-checkout/provider.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-1"
+
+  assume_role {
+    role_arn    = var.target_deployment_role
+    external_id = "AWS_DEVOPS_DEPLOYMENT"
+  }
+}
+
+  # terraform {
+  #   backend "s3" {
+  #     bucket = "greenn-devops-state"
+  #     key    = "greenn-pci-prd/container.tfstate"
+  #     region = "us-east-1"
+  #   }
+  # }
+
+terraform {
+  backend "s3" {
+    bucket = "greenn-devops-state"
+    key    = "greenn-pci-stg/new-checkout/state.tfstate"
+    region = "us-east-1"
+  }
+}
---
+++ infrastructure/stg/new-checkout/variables.tf
@@ -0,0 +1,68 @@
+variable "nginx_default_image" {
+  type = string
+}
+
+variable "php_default_image" {
+  type = string
+}
+
+variable "container_security_group" {
+  type = list(any)
+}
+
+variable "cpu" {
+  type    = number
+  default = 2048
+}
+
+variable "target_deployment_role" {
+  type = string
+}
+
+variable "application_host_header_values" {
+  type = list(any)
+}
+
+variable "memory" {
+  type    = number
+  default = 4096
+}
+
+variable "name" {
+  type = string
+}
+
+variable "application" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "alb_https_listener" {
+  type = string
+}
+
+variable "app_subnets" {
+  type = list(any)
+}
+
+variable "environment" {
+  type    = string
+  default = "production"
+}
+
+variable "max_capacity" {
+  type    = number
+  default = 20
+}
+
+variable "min_capacity" {
+  type    = number
+  default = 2
+}
+
+variable "cluster_name" {
+  type = string
+}
