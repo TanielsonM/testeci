b0a88013527b1ffe5dfe2cd2d91b178627971308
Caio Bolognese <caio.bolognese@greenn.com.br>
2024-07-26 14:20:56 -0300

    Fix: ajuste na validação dentro do organizers

--- components/Event/Organizers.vue
+++ components/Event/Organizers.vue
@@ -7,7 +7,7 @@ const { product } = useProductStore();
   <div>
     <p class="text-[16px] font-[700] text-main-color mb-3">{{ $t("pre_checkout.organizers") }}</p>
     <p class="text-[16px] font-[400] leading-relaxed text-input-color mb-3">
-      {{ (product?.seller?.company ? product?.seller?.company?.fantasy_name : product?.seller?.name).toUpperCase() }}
+      {{ (product?.seller?.company?.fantasy_name ? product?.seller?.company?.fantasy_name : product?.seller?.name).toUpperCase() }}
     </p>
   </div>
 </template>
